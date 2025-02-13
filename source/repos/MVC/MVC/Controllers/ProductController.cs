6c12778d7dd0d691850e466580b761d272fd245b
bethahemanth <bethahemanth7264@gmail.com>
2025-02-13 10:42:50 +0530

    hello

---
+++ source/repos/MVC/MVC/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> l = new List<Product>();
+            l.Add(new Product { Id = 1, Name = "Product 1", Price = 100 });
+            l.Add(new Product { Id = 2, Name = "Product 2", Price = 200 });
+            return View(l);
+        }
+    }
+}
